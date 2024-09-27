5a38cb17fd24787bb0bca84787dd26e9039275bd
CosminF21 <cosmin.cofan@alten.com>
2024-09-27 09:13:47 +0300

    add logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,4 +34,8 @@ export class AppComponent implements OnInit{
   get isAuthenticated(){
     return this.oidcSecurityService.isAuthenticated();
   }
+
+  logout(){
+    this.oidcSecurityService.logoff().subscribe();
+  }
 }
